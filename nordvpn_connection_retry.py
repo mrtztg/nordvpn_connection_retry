c4614a743ef0ab1d3b7fdca39912acda43b298d7
morteza <mrtztg@gmail.com>
2021-06-11 23:15:33 +0430

    Initial commit

---
+++ nordvpn_connection_retry.py
@@ -0,0 +1,19 @@
+from random import shuffle
+from subprocess import run, PIPE
+
+if __name__ != '__main__':
+    exit()
+countries_cmd = 'nordvpn countries'.split()
+countries_output = run(countries_cmd, stdout=PIPE, stderr=PIPE, check=True, text=True,
+                       universal_newlines=True)
+countries = list(filter(lambda x: len(x) > 1, countries_output.stdout.split()))
+shuffle(countries)
+for country in countries:
+    connect_cmd = f'nordvpn c {country}'.split()
+    connect_output = run(connect_cmd, stdout=PIPE, stderr=PIPE, check=False, text=True,
+                         universal_newlines=True)
+    if connect_output.returncode == 0:
+        print(f'connected to {country}')
+        break
+    print('stdout:' + connect_output.stdout)
+    print('stderr:' + connect_output.stderr)
